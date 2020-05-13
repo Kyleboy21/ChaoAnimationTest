6c48af1093226814e03eba6f6805dfcf13023832
Kyleboy21 <65284230+Kyleboy21@users.noreply.github.com>
2020-05-13 11:51:46 -0400

    Rename stdafx.h to ChaoAnimationTest/stdafx.h

---
+++ ChaoAnimationTest/stdafx.h
@@ -0,0 +1,23 @@
+﻿// stdafx.h : include file for standard system include files,
+// or project specific include files that are used frequently, but
+// are changed infrequently
+//
+
+#pragma once
+
+#include "targetver.h"
+
+#define WIN32_LEAN_AND_MEAN             // Exclude rarely-used stuff from Windows headers
+// Windows Header Files:
+#include <windows.h>
+#include <memory>
+#include <string>
+#include <unordered_map>
+#include <unordered_set>
+#include <cstdint>
+#include <fstream>
+#include <iostream>
+
+
+
+// TODO: reference additional headers your program requires here
